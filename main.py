f337eff9312ebb562a27dcb3677964fc18904b00
Aaron Olinger <aaron.olinger@ncf.edu>
2013-09-18 22:04:09 -0400

    Commented main and removed useless file util.pyc

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#	Main function for Cellular II
+#	Version 1.1
+			
+#	DEPENDENCIES: pygame, cffi	
+
+#=====Import List=====#
 import environment
 import food
 import test
@@ -6,41 +12,64 @@ import copy
 import sys
 import display
 import pygame
+#====End of Imports===#
 
 def main():
-	if len(sys.argv) == 2 and sys.argv[1] == '-':
-		starting_food_count = 100
+	"""Runs Cellular II"""
+	#Retrieve user input from command line
+ 	
+#	If dash in is input as first arg to Cellular II, default to 100 for food and cell count
+
+#	System arguments numbering starts from python command, e.g. { python Cellular-II.py $foo $bar } 
+#	where $1 is Cellular-II.py $2 is $foo and $3 is $bar
+
+	if len(sys.argv) == 2 and sys.argv[1] == '-':	#Evaluates for the case: { python Cellular-II.py - } (one arg && arg=='-')
+		starting_food_count = 100		
 		starting_cell_count = 100
-	elif len(sys.argv) == 3:
+
+	elif len(sys.argv) == 3:			#Evaluates for the case: { python Cellular-II.py $2 $3 } (two args)
 		starting_food_count  = int(sys.argv[1])
 		starting_cell_count  = int(sys.argv[2])
-	else:
+	else:						#Evaluates for the case { python Cellular-II.py } (no args) unexpected number of args
 		starting_food_count = input('Enter starting amount of food: ')
 		starting_cell_count = input('Enter starting amount of cells: ')
+
+#	Initialize World Object	
 	World = environment.Environment(starting_food_count,starting_cell_count)
 	
-	# dis is a thread
+# 	Where dis is a thread 
 	dis = display.display(World)
-	worldClock = pygame.time.Clock()
 
-	# i is a tick counter
-	i = 0
+#	Initial Tick
+	worldClock = pygame.time.Clock()
+	
+	i = 0						#Tick Counter
+
+#						###MAIN LOOP BEGIN###
 	while True:
 		i += 1
-		# if the user exited pygame, close the rest of the program
-		if dis.isAlive() == False:
+#		If the user closes pygame, close the rest of the program
+		if dis.isAlive() == False:		#If thread is stopped, isAlive() evaluates False
 			sys.exit()
 
+#		Terminal output	
 		print 'Tick:',i,'\t\tfood: ',len(World.food_set),'\t\tcells: ',len(World.cell_list)
 		print 'Resistance: ', environment.Environment().resistance
-	
-		#print 'Tick: ',i,'\t\tfood: ',len(World.food_set),'\t\tcells: ',len(World.cell_list)
-		#print 1000/
+
+#					###----------Obsolete Code----------###
+#		#print 'Tick: ',i,'\t\tfood: ',len(World.food_set),'\t\tcells: ',len(World.cell_list)
+#		#print 1000/
+#					###--------END Obsolete Code--------###
+
+
 		(worldClock.tick(60) + 0.00000000001)
 		World.tick()
 		World.print_table("Main_Test.txt","Tick: "+str(i))
+#						###MAIN LOOP END###
 
-	# if the main loop is over, close the graphics thread
+# 	Closes the graphics thread if the main loop is broken
 	dis._Thread__stop()
-		
+
+
+#Runs above function	
 main()
