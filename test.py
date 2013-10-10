032fe938f8b45004c011cfba829d1df9ad1779e0
Brad <brad@ubuntu.(none)>
2013-10-09 22:14:02 -0400

    further changes to test.py by Ben Carothers, Brad Baker, and Peter Gui first half of code: (def setup - def iadd) written by David W., Brad Baker, and Ben C. second half of code: (def add - def main) written by Brad B., Peter G., and Ben C.
    Still some issues with running the tests - variables initiated in Setup need to be made global

--- test.py
+++ test.py
@@ -1,18 +1,18 @@
 import unittest, food, environment, cells, vector
 
-if __name__ == "__main__":
-	environment.Environment(10, 10)
-	#environment.Environment(10, 10) #this should fail
-	unittest.main()
+##if __name__ == "__main__":
+##        environment.Environment(10, 10)
+##        #environment.Environment(10, 10) #this should fail
+##        unittest.main()
 
 class TestVector(unittest.TestCase):
-	def setup(self):
-		import environment
-		environment = environment.Environment(10,10)
-		max_width = environment.width 
-		max_height = environment.height
-		# set x and y pairs
-		#silly rabbit trix are for kids
+        def setUp(self):
+                #import environment
+                #environment3 = environment.Environment(10,10)
+                max_width = environment3.width 
+                max_height = environment3.height
+                # set x and y pairs
+                #silly rabbit trix are for kids
                 x1 = 0
                 x2 = 1
                 y1 = 0
@@ -21,43 +21,43 @@ class TestVector(unittest.TestCase):
                 self.unitVector = vector.Vector(x2,y2)
                 self.v = vector.Vector(.2,.1)
                 
-	def test_distance(self):
-		# test if distance(testpoints= known distance
-		self.assertEqual(round(distance(0,0,.9,.9),3),1.273)
-		self.assertEqual(distance(.9,.9,0,0),0)
-		self.assertEqual(distance(.5,.3,.2,.7),.5)
-		self.assertEqual(round((distance(.4,.3,.2,.7),5)),.44721)
-		self.assertEqual(distance(.3,.2,.3,.2),0)
-		self.assert_false(distance(0,0,self.width,self.height) == math.sqrt(2))
+        def test_distance(self):
+                # test if distance(testpoints= known distance
+                self.assertEqual(round(distance(0,0,.9,.9),3),1.273)
+                self.assertEqual(distance(.9,.9,0,0),0)
+                self.assertEqual(distance(.5,.3,.2,.7),.5)
+                self.assertEqual(round((distance(.4,.3,.2,.7),5)),.44721)
+                self.assertEqual(distance(.3,.2,.3,.2),0)
+                self.assert_false(distance(0,0,self.width,self.height) == math.sqrt(2))
 
-		
-	def test_diff(self):
-		# test if diff(testpoints= known distance
-		self.assertEqual(diff(0,0),0)
-		dif(0,0) ==0 ? 
-		self.assertEqual(diff(1,1),0)
-		self.assertEqual(diff(.5,.3).2)
-		self.assertEqual(diff(.9,.25),.65)
-
-	def test__init__(self):
-		newV = Vector(0,1)
-		newP = Point(Vector)
-		self.assertEqual(newV.y,1)
-		self.assertEqual(newV.x,0)
-
-	def test__iadd__(self):
                 
-		vTemp = v #storing the default in temporary variable vTemp
-		v2 = Vector(.3,.2)
-		v += v2
-		self.assertEqual((v.x,.5))
-		self.assertEqual((v.y,.3))
+        def test_diff(self):
+                # test if diff(testpoints= known distance
+                self.assertEqual(diff(0,0),0)
+                #dif(0,0) ==0 ? 
+                self.assertEqual(diff(1,1),0)
+                self.assertEqual(diff(.5,.3),.2)
+                self.assertEqual(diff(.9,.25),.65)
+
+        def test__init__(self):
+                newV = Vector(0,1)
+                newP = Point(Vector)
+                self.assertEqual(newV.y,1)
+                self.assertEqual(newV.x,0)
+
+        def test__iadd__(self):
+                
+                vTemp = v #storing the default in temporary variable vTemp
+                v2 = Vector(.3,.2)
+                v += v2
+                self.assertEqual((v.x,.5))
+                self.assertEqual((v.y,.3))
         
                 v = vTemp #resetting v to default value, stored in vTemp
-		v3 = Vector(0,0)
-		v += v3
-		self.assertEqual((v.x,.2))
-		self.assertEqual((v.x,.1))
+                v3 = Vector(0,0)
+                v += v3
+                self.assertEqual((v.x,.2))
+                self.assertEqual((v.x,.1))
 
                 v = vTemp #resetting for further testing 
 
@@ -95,31 +95,39 @@ class TestVector(unittest.TestCase):
 
         def test_neg(self):
                 self.negVector = -v
-                assertEqual(negVector.x, -v.x)
-                assertEqual(negVector.y, -v.y)
+                self.assertEqual(negVector.x, -v.x)
+                self.assertEqual(negVector.y, -v.y)
                 negVector = -unitVector
-                assertEqual(negVector.x, -1)
-                assertEqual(negVector.y, -1)
+                self.assertEqual(negVector.x, -1)
+                self.assertEqual(negVector.y, -1)
 
         def test_abs(self):
-                assertEqual(abs(unitVector), math.sqrt(2))
-                assertEqual(abs(nullVector), 0)
-                assertEqual(round(abs(v),3), 0.224) 
+                self.assertEqual(abs(unitVector), math.sqrt(2))
+                self.assertEqual(abs(nullVector), 0)
+                self.assertEqual(round(abs(v),3), 0.224) 
 
         def test_repr(self):
                 vectName = str(unitVector)
-                assertEqual(vectName, '{1,1)')
+                self.assertEqual(vectName, '{1,1)')
                 vectName = str(nullVector)
-                assertEqual(vectName, '(0,0)')
+                self.assertEqual(vectName, '(0,0)')
                 vectName = str(v)
-                assertEqual(vectName, '(.2,.1)')
+                self.assertEqual(vectName, '(.2,.1)')
 
         def test_eq(self):
                 testVector = vector.Vector(1,1)
-                assertEqual(testVector == unitVector, True)
+                self.assertEqual(testVector == unitVector, True)
                 testVector = vector.Vector(0,0)
-                assertEqual(testVector == nullVector, True)
-                
+                self.assertEqual(testVector == nullVector, True)
+
+##        def test_fit_to_torus(self):
+##                if not (self.x>=0 and self.x<1):
+##                        fail()
+##                if not (self.y>=0 and self.y<1):
+##                        fail()
         
                 
-                
+if __name__ == '__main__':
+        #environment.Environment(10, 10)
+        environment3 = environment.Environment(10,10)
+        unittest.main()
