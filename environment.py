55975f2b3bfd8d6d0d941b1039e4562c441ea10c
Dalen W. Brauner <DalenWBrauner@Gmail.com>
2013-10-23 21:11:52 -0400

    Progress made last class. It runs, at least.

--- environment.py
+++ environment.py
@@ -4,174 +4,177 @@ if __name__ == "__main__": print 'no'; exit(-1)
 import cells, food, random, unittest, singleton, vector, threading
 
 class Environment(singleton.Singleton):
-	def init_once(self, food_count, cells_count, add_food_rate, usr_resist):
-		"""Generate a 1x1 environment with specified amount of food and cells"""
-		self.cell_list = []
-		self.food_set = set()
-		self.lock = threading.Lock()
-		self.width = self.height = 1.0
-		self.add_food(food_count)
-		self.add_cells(cells_count)
-		self.turn = 0
-		self.reseed_prob = add_food_rate
-		self.resistance = usr_resist
+##      def init_once(self, food_count, cells_count, add_food_rate, usr_resist):
+        def init_once(self, food_count, cells_count):
+                """Generate a 1x1 environment with specified amount of food and cells"""
+                self.cell_list = []
+                self.food_set = set()
+                self.lock = threading.Lock()
+                self.width = self.height = 1.0
+                self.add_food(food_count)
+                self.add_cells(cells_count)
+                self.turn = 0
+                self.reseed_prob = 10
+                self.resistance = 600
+##                self.reseed_prob = add_food_rate
+##                self.resistance = usr_resist
 
-	def add_food(self, food_count):
-		"""Add food_count number of foods at random locations"""
-		for i in range(food_count):
-			self.food_set.add(food.Food(random.uniform(0, self.width), random.uniform(0, self.height)))
-	def add_food_at_location(self, pos):
-		"""Add a food item at location"""
-		self.food_set.add(food.Food(pos.x, pos.y))
+        def add_food(self, food_count):
+                """Add food_count number of foods at random locations"""
+                for i in range(food_count):
+                        self.food_set.add(food.Food(random.uniform(0, self.width), random.uniform(0, self.height)))
+        def add_food_at_location(self, pos):
+                """Add a food item at location"""
+                self.food_set.add(food.Food(pos.x, pos.y))
 
-	def add_cells(self, cell_count):
-		''' Add cell_count number of cells of random colors at random locations to the world'''
-		for i in range(cell_count):
-			self.cell_list.append(cells.Cell(random.uniform(0, self.width), random.uniform(0, self.height)))
-			
-	def add_cell_at_location(self, pos):
-		"""Add a cell at location"""
-		self.cell_list.append(cells.Cell(pos.x, pos.y))
-			
-	def tick(self):
-		''' give each cell a turn and maybe add food to the world'''
-		# we need to lock the cell_list so that we can itterate through it
-		self.lock.acquire()
+        def add_cells(self, cell_count):
+                ''' Add cell_count number of cells of random colors at random locations to the world'''
+                for i in range(cell_count):
+                        self.cell_list.append(cells.Cell(random.uniform(0, self.width), random.uniform(0, self.height)))
+                        
+        def add_cell_at_location(self, pos):
+                """Add a cell at location"""
+                self.cell_list.append(cells.Cell(pos.x, pos.y))
+                        
+        def tick(self):
+                ''' give each cell a turn and maybe add food to the world'''
+                # we need to lock the cell_list so that we can itterate through it
+                self.lock.acquire()
 
-		for cell in self.cell_list:
-			cell.one_tick()
-			
-		# There is reseed_prob chance that a food item is added to the word at a random place.
-		if random.randint(0,100)<=self.reseed_prob:
-			self.add_food(1)
-		self.turn += 1
-		
-		# maybe we can move this up before food is added?
-		self.lock.release()
+                for cell in self.cell_list:
+                        cell.one_tick()
+                        
+                # There is reseed_prob chance that a food item is added to the word at a random place.
+                if random.randint(0,100)<=self.reseed_prob:
+                        self.add_food(1)
+                self.turn += 1
+                
+                # maybe we can move this up before food is added?
+                self.lock.release()
 
-	def food_at(self, pos, r):
-		'''return list of food within distance r of position pos'''
-		return [food for food in self.food_set if pos.distance_to(food.pos) <= r]
+        def food_at(self, pos, r):
+                '''return list of food within distance r of position pos'''
+                return [food for food in self.food_set if pos.distance_to(food.pos) <= r]
 
-	def remove_food(self, food):
-		'''delete a food item'''
-		try:
-			self.food_set.remove(food)
+        def remove_food(self, food):
+                '''delete a food item'''
+                try:
+                        self.food_set.remove(food)
 
-		# handle food having already been removed
-		except KeyError:
-			pass
+                # handle food having already been removed
+                except KeyError:
+                        pass
 
-	def remove_cell(self,cell):
-		''' delete a cell'''
-		self.cell_list.remove(cell)
-		
-	def kill_cell(self,cell):
-		''' replaces a cell with food'''
-		self.cell_list.remove(cell)
-		self.add_food_at_location(cell.pos)
+        def remove_cell(self,cell):
+                ''' delete a cell'''
+                self.cell_list.remove(cell)
+                
+        def kill_cell(self,cell):
+                ''' replaces a cell with food'''
+                self.cell_list.remove(cell)
+                self.add_food_at_location(cell.pos)
 
-	# print_table()
-	#	output a table of each cell state to a text file
+        # print_table()
+        #       output a table of each cell state to a text file
 
-	def print_table(self,filename,comment=""):
-		"""Prints a table to a textfile with the provided name, with the provided comment above it."""
-		''' Data: Cell placement, velocity, acceleration destination, radius, energy, mass, task'''
-		table_file = open(filename,"a")
-		# Header
-		table_file.write("\n"+str(comment)+"\nCell_n\tx_pos\ty_pos\tx_vel\ty_vel\tx_acl\ty_acl\tx_dest\ty_dest\tradius\tenergy\tmass\ttask\n")
-		counter = 0
-		for cell in self.cell_list:
-			# Beginning of the row
-			table_file.write("Cell_"+str(counter)+"\t"+str(round(cell.pos.x,4))+"\t"+str(round(cell.pos.y,4))+"\t"+\
-			str(round(cell.vel.x,4))+"\t"+str(round(cell.vel.y,4))+"\t"+str(round(cell.acl.x,4))+"\t"+str(round(cell.acl.y,4)))
+        def print_table(self,filename,comment=""):
+                """Prints a table to a textfile with the provided name, with the provided comment above it."""
+                ''' Data: Cell placement, velocity, acceleration destination, radius, energy, mass, task'''
+                table_file = open(filename,"a")
+                # Header
+                table_file.write("\n"+str(comment)+"\nCell_n\tx_pos\ty_pos\tx_vel\ty_vel\tx_acl\ty_acl\tx_dest\ty_dest\tradius\tenergy\tmass\ttask\n")
+                counter = 0
+                for cell in self.cell_list:
+                        # Beginning of the row
+                        table_file.write("Cell_"+str(counter)+"\t"+str(round(cell.pos.x,4))+"\t"+str(round(cell.pos.y,4))+"\t"+\
+                        str(round(cell.vel.x,4))+"\t"+str(round(cell.vel.y,4))+"\t"+str(round(cell.acl.x,4))+"\t"+str(round(cell.acl.y,4)))
 
-			# Destination issues; need the if/elif for when destination is just Nonetype
-			if type(cell.destination) == type(None):
-				table_file.write("None\tNone\t")
-			elif cell.destination.__class__ == vector.Point:
-				table_file.write(str(round(cell.destination.x,4))+"\t"+str(round(cell.destination.y,4))+"\t")
-			else: raise TypeError(str(type(cell.destination))+" "+str(cell.destination))
+                        # Destination issues; need the if/elif for when destination is just Nonetype
+                        if type(cell.destination) == type(None):
+                                table_file.write("None\tNone\t")
+                        elif cell.destination.__class__ == vector.Point:
+                                table_file.write(str(round(cell.destination.x,4))+"\t"+str(round(cell.destination.y,4))+"\t")
+                        else: raise TypeError(str(type(cell.destination))+" "+str(cell.destination))
 
-			# Back to the rest of the row
-			table_file.write("%7.3f %7.3f %7.3f" %  (cell.radius, cell.energy, cell.mass))
-			#table_file.write(str(round(cell.radius,4))+"\t"+str(round(cell.energy,4))+"\t"+str(round(cell.mass,4))+\
-			#"\t"+str(cell.task)+"\n")
+                        # Back to the rest of the row
+                        table_file.write("%7.3f %7.3f %7.3f" %  (cell.radius, cell.energy, cell.mass))
+                        #table_file.write(str(round(cell.radius,4))+"\t"+str(round(cell.energy,4))+"\t"+str(round(cell.mass,4))+\
+                        #"\t"+str(cell.task)+"\n")
 
-			counter += 1
-		table_file.close()
+                        counter += 1
+                table_file.close()
 
 class CreationTest(unittest.TestCase):
-	''' unit tests'''
-	def runTest(self):
-		''' run unit tests'''
-		#environment already initialized in test.py
-		environment = Environment() 
+        ''' unit tests'''
+        def runTest(self):
+                ''' run unit tests'''
+                #environment already initialized in test.py
+                environment = Environment() 
 
-		# test that environment is a singleton
-		self.assertTrue(Environment() is environment)
+                # test that environment is a singleton
+                self.assertTrue(Environment() is environment)
 
-		# test that environment initializes properly
-		self.assertEquals(len(environment.cell_list), 10)
-		self.assertEquals(len(environment.food_set), 10)
-		self.assertTrue(environment.width > 0)
-		self.assertTrue(environment.height > 0)
-		
-		# test that cells are within bounds
-		for cell in environment.cell_list:
-			self.assertTrue(cell.pos.x >= 0 and cell.pos.x <= environment.width and cell.pos.y >= 0 and cell.pos.y <= environment.height, "Cell location out of bounds.")
-		# ..and food is within bounds
-		for f in environment.food_set:
-			self.assertTrue(f.pos.x >= 0 and f.pos.x <= environment.width and f.pos.y >= 0 and f.pos.y <= environment.height, "Food location out of bounds.")
+                # test that environment initializes properly
+                self.assertEquals(len(environment.cell_list), 10)
+                self.assertEquals(len(environment.food_set), 10)
+                self.assertTrue(environment.width > 0)
+                self.assertTrue(environment.height > 0)
+                
+                # test that cells are within bounds
+                for cell in environment.cell_list:
+                        self.assertTrue(cell.pos.x >= 0 and cell.pos.x <= environment.width and cell.pos.y >= 0 and cell.pos.y <= environment.height, "Cell location out of bounds.")
+                # ..and food is within bounds
+                for f in environment.food_set:
+                        self.assertTrue(f.pos.x >= 0 and f.pos.x <= environment.width and f.pos.y >= 0 and f.pos.y <= environment.height, "Food location out of bounds.")
 
-		environment.cell_list = []
-		# test that a cell will find and eat food underneath it
-		c = cells.Cell(environment.width/2, environment.height/2)
-		environment.cell_list.append(c)
-		food_count = len(environment.food_set)
+                environment.cell_list = []
+                # test that a cell will find and eat food underneath it
+                c = cells.Cell(environment.width/2, environment.height/2)
+                environment.cell_list.append(c)
+                food_count = len(environment.food_set)
 
-		environment.food_set.add(food.Food(environment.width/2, environment.height/2))		
-		environment.tick()
-		# test that food list count was decremented after food is eaten
-		self.assertEqual(len(environment.food_set), food_count) 
-		
-		# test that food inside the cell is eaten
-		environment.food_set.add(food.Food(environment.width/2 + c.radius - 0.000001, environment.height/2))
-		environment.tick()
-		self.assertEqual(len(environment.food_set), food_count)
+                environment.food_set.add(food.Food(environment.width/2, environment.height/2))          
+                environment.tick()
+                # test that food list count was decremented after food is eaten
+                self.assertEqual(len(environment.food_set), food_count) 
+                
+                # test that food inside the cell is eaten
+                environment.food_set.add(food.Food(environment.width/2 + c.radius - 0.000001, environment.height/2))
+                environment.tick()
+                self.assertEqual(len(environment.food_set), food_count)
 
-		# test that food outside the cell is not eaten
-		environment.food_set.add(food.Food(environment.width/2 + c.radius + 0.000001, environment.height/2))
-		environment.tick()
-		self.assertEqual(len(environment.food_set), food_count + 1)
-		
-		# test that add_cells adds the right number of cells
-		num_cells = len(environment.cell_list)
-		add_cells_count = random.randint(0,100)
-		environment.add_cells(add_cells_count)
-		self.assertEqual(len(environment.cell_list)-add_cells_count,num_cells)
-		
+                # test that food outside the cell is not eaten
+                environment.food_set.add(food.Food(environment.width/2 + c.radius + 0.000001, environment.height/2))
+                environment.tick()
+                self.assertEqual(len(environment.food_set), food_count + 1)
+                
+                # test that add_cells adds the right number of cells
+                num_cells = len(environment.cell_list)
+                add_cells_count = random.randint(0,100)
+                environment.add_cells(add_cells_count)
+                self.assertEqual(len(environment.cell_list)-add_cells_count,num_cells)
+                
 
 def debug_print_table():
-	'''tests print_table by creating a world with set information and then print_tabling that world to make sure the info is accurate'''
-	tbl = "Debug_Cell_Table.txt"
-	Env = Environment(0,0)
-	Env.cell_list.append(Cell(0,0))
-	Env.cell_list.append(Cell(1,2))
-	Env.cell_list.append(Cell(7,5))
-	Env.cell_list.append(Cell(6,9))
-	Env.cell_list.append(Cell(-7,-7))
-	Env.print_table(tbl)
-	Env.tick()
-	Env.print_table(tbl)
-	Env.tick()
-	Env.print_table(tbl)
-	for run in xrange(5):
-		Env.tick()
-	Env.print_table(tbl)
-	for run in xrange(50):
-		Env.tick()
-	Env.print_table(tbl)
+        '''tests print_table by creating a world with set information and then print_tabling that world to make sure the info is accurate'''
+        tbl = "Debug_Cell_Table.txt"
+        Env = Environment(0,0)
+        Env.cell_list.append(Cell(0,0))
+        Env.cell_list.append(Cell(1,2))
+        Env.cell_list.append(Cell(7,5))
+        Env.cell_list.append(Cell(6,9))
+        Env.cell_list.append(Cell(-7,-7))
+        Env.print_table(tbl)
+        Env.tick()
+        Env.print_table(tbl)
+        Env.tick()
+        Env.print_table(tbl)
+        for run in xrange(5):
+                Env.tick()
+        Env.print_table(tbl)
+        for run in xrange(50):
+                Env.tick()
+        Env.print_table(tbl)
 
 
 
--- test.py
+++ test.py
@@ -1,8 +1,6 @@
 
 import unittest, food, environment, cells, vector, random, math
 
-
-
 #Tests for Environment
 #EnvironmentCreationTest = environment.CreationTest
 
@@ -18,38 +16,35 @@ global_VecsunitVector = vector.Vector(global_Vecsx2,global_Vecsy2)
 global_Vecsv = vector.Vector(.2,.1)
 
 class TestEnvironment(unittest.TestCase):
-	#Initilization
+        def setUp(self):
+                #Resets Test Environment global_e
+                global_e.init_once(10,10)     
+        
+        def test_init_once(self):
+                global_e.init_once(10,10)       
+                self.assertTrue(global_e.cell_list)
+                self.assertEquals(global_e.width, global_e.height,1.0)
+                self.assertEquals(global_e.turn, 0)
+                self.assertEquals(global_e.reseed_prob,10)
+                self.assertEquals(global_e.resistance, 600)
+                self.assertEquals(len(global_e.food_set), 10)
+                print "init_once is a go"
 
-	def setUp(self):
-		#Resets Test Environment global_e
-		global_e.init_once(10,10)
-		
-	
-	def test_init_once(self):
-		global_e.init_once(10,10)	
-		self.assertTrue(global_e.cell_list)
-		self.assertEquals(global_e.width, global_e.height,1.0)
-		self.assertEquals(global_e.turn, 0)
-		self.assertEquals(global_e.reseed_prob,10)
-		self.assertEquals(global_e.resistance, 600)
-		self.assertEquals(len(global_e.food_set), 10)
-		print "init_once is a go"
-
-	def test_add_food(self):
-		fc=len(global_e.food_set)
-		global_e.add_food(10)
-		self.assertEquals(len(global_e.food_set), fc+10)
-		print "add_food is a go"
+        def test_add_food(self):
+                fc=len(global_e.food_set)
+                global_e.add_food(10)
+                self.assertEquals(len(global_e.food_set), fc+10)
+                print "add_food is a go"
 
 #Not yet finished
-	def test_add_food_at_location(self):
-		global_e.cell_list=[]
-		test_cell=cells.Cell(0.1,0.2)
-		global_e.cell_list.append(cells.Cell(0.1, 0.2))
-		self.assertEquals(global_e.cell_list[0].pos.distance_to(test_cell.pos), 0.0)
-		print "add_food_at_location is a go"
-	
-	
+        def test_add_food_at_location(self):
+                global_e.cell_list=[]
+                test_cell=cells.Cell(0.1,0.2)
+                global_e.cell_list.append(cells.Cell(0.1, 0.2))
+                self.assertEquals(global_e.cell_list[0].pos.distance_to(test_cell.pos), 0.0)
+                print "add_food_at_location is a go"
+        
+        
 #Tests for Cells
 #CellsCreationTest = cells.TestFunctions
 
@@ -101,12 +96,14 @@ class TestVector(unittest.TestCase):
                 self.assertEqual(newV.x,0)
 
         def test__iadd__(self):
-                
+                print "In the garden of eden",global_VecsunitVector
+                print "test_iadd which involves 0",global_Vecsv
                 vTemp = global_Vecsv #storing the default in temporary variable vTemp
                 v2 = Vector(.3,.2)
                 global_Vecsv += v2
                 self.assertEqual((global_Vecsv.x,.5))
                 self.assertEqual((global_Vecsv.y,.3))
+                print "test_iadd which involves 1",global_Vecsv
         
                 global_Vecsv = vTemp #resetting v to default value, stored in vTemp
                 v3 = Vector(0,0)
@@ -114,8 +111,10 @@ class TestVector(unittest.TestCase):
                 self.assertEqual((global_Vecsv.x,.2))
                 self.assertEqual((global_Vecsv.x,.1))
                 global_Vecsv = vTemp
+                print "test_iadd which involves 2",global_Vecsv
                 
         def test_add(self):
+                print "test_add which involves",global_Vecsv
                 addResult = global_VecsunitVector + global_VecsnullVector
                 if not(addResult == global_VecsunitVector):
                         fail()
@@ -183,24 +182,24 @@ class TestVector(unittest.TestCase):
 
 
 class TestFood(unittest.TestCase):
-	
-	def setUp(self):
-		self.x=1.3
-		self.y=5.7
-		self.foodObj = food.Food(self.x,self.y)
+        
+        def setUp(self):
+                self.x=1.3
+                self.y=5.7
+                self.foodObj = food.Food(self.x,self.y)
 
-	def test_foodStats(self):
-		self.assertEqual(self.foodObj.energy, 0.5)
-		self.assertEqual(self.foodObj.pos.x, self.x%1)
-		self.assertEqual(self.foodObj.pos.y, self.y%1)
+        def test_foodStats(self):
+                self.assertEqual(self.foodObj.energy, 0.5)
+                self.assertEqual(self.foodObj.pos.x, self.x%1)
+                self.assertEqual(self.foodObj.pos.y, self.y%1)
 
 
 class TestCells(unittest.TestCase):
-	def setUp(self):
-		self.environmentObj = environment.Environment(10,10)
-		self.cellList = self.environmentObj.cell_list
-		
-	def test_init(self):
+        def setUp(self):
+                self.environmentObj = global_e
+                self.cellList = self.environmentObj.cell_list
+                
+        def test_init(self):
                 pheno = [2.0,.001,.5,.6,.005,None,0]
                 
                 Alfred = cells.Cell(.1,.9,.11,7)
@@ -256,7 +255,7 @@ class TestCells(unittest.TestCase):
                 self.assertEqual(Dingus.color , "F")
                 self.assertEqual(Dingus.mutation_chance , "G")
                 
-	def test_task_none(self):
+        def test_task_none(self):
                 Alph = cells.Cell(0,0)
                 self.assertEqual(Alph.task , None)
                 Alph.task_none()
@@ -264,35 +263,38 @@ class TestCells(unittest.TestCase):
                 Alph.task = None
                 Alph.TaskTable[Alph.task]()
                 self.assertEqual(Alph.task , "FindingFood")
-		
-	def test_life_and_death(self):
-	#uncomment print statements to see live output
-		babies = 0
-		corpses = 0
-		duplicate = list(self.cellList)
-		for cell in duplicate:
-			cell.div_mass = .3
-			cell.div_energy = .3
-			cell.emRation = 2
-			cell.mass = round(random.uniform(.0,.5),2)
-			cell.energy = round(random.uniform(.0,.5),2)
-			orig = len(self.cellList)
-			cell.life_and_death()
-			if cell.mass>= cell.div_mass and cell.energy >= cell.div_energy:
-				self.assertEqual(len(self.cellList),(orig+1))
-				corpses+=1
-				babies+=2
-				#print "A cell of mass",cell.mass,"has been sacrificed to the fertility gods.\n"
-			elif cell.mass <= 0.1:
-				self.assertEqual(len(self.cellList),(orig-1))
-				corpses+=1
-				#print "A cell of mass",cell.mass,"has died of starvation.\n"
-			else:
-				self.assertEqual(len(self.cellList),(orig))
-				#print "A cell of mass",cell.mass,"has lived to fight another day.\n"
-		#print "\n",babies,"new cells were made and",corpses,"cells died."
-		#print len(self.cellList),"cells remain."
-	
+
+        def test_task_finding_food(self):
+                
+                
+        def test_life_and_death(self):
+        #uncomment print statements to see live output
+                babies = 0
+                corpses = 0
+                duplicate = list(self.cellList)
+                for cell in duplicate:
+                        cell.div_mass = .3
+                        cell.div_energy = .3
+                        cell.emRation = 2
+                        cell.mass = round(random.uniform(.0,.5),2)
+                        cell.energy = round(random.uniform(.0,.5),2)
+                        orig = len(self.cellList)
+                        cell.life_and_death()
+                        if cell.mass>= cell.div_mass and cell.energy >= cell.div_energy:
+                                self.assertEqual(len(self.cellList),(orig+1))
+                                corpses+=1
+                                babies+=2
+                                #print "A cell of mass",cell.mass,"has been sacrificed to the fertility gods.\n"
+                        elif cell.mass <= 0.1:
+                                self.assertEqual(len(self.cellList),(orig-1))
+                                corpses+=1
+                                #print "A cell of mass",cell.mass,"has died of starvation.\n"
+                        else:
+                                self.assertEqual(len(self.cellList),(orig))
+                                #print "A cell of mass",cell.mass,"has lived to fight another day.\n"
+                #print "\n",babies,"new cells were made and",corpses,"cells died."
+                #print len(self.cellList),"cells remain."
+        
 if __name__ == '__main__':
         unittest.main()
 
