8c1753d9291367927c8dfe14bcb5305e583b3695
LandoSama <lando_lakes@rocketmail.com>
2013-10-28 21:28:23 -0400

    indent errors

--- cells.py
+++ cells.py
@@ -154,14 +154,14 @@ class Cell:
 		
 		#This is for the variation
 		 	
-		    randomvariation = random.uniform(0,.5) #Picks a random float between 0 and .005
-		    if self.phenotype[0] - randomvariation <= 1:   #If subtracting the value would cause the phenotype to be negative it just adds it
-		        self.phenotype[0] += randomvariation
-		    else:
-		        direction =  random.randint(-1,1)           #Otherwise, it picks an integer between -1 and 1
-		        randomvariation = randomvariation * direction      #Then multiplies it by the float
-		        self.phenotype[0] += randomvariation                                              #And adds that value
-		    newphenotype.append(self.phenotype[0])
+		randomvariation = random.uniform(0,.5) #Picks a random float between 0 and .005
+		if self.phenotype[0] - randomvariation <= 1:   #If subtracting the value would cause the phenotype to be negative it just adds it
+		    self.phenotype[0] += randomvariation
+		else:
+		    direction =  random.randint(-1,1)           #Otherwise, it picks an integer between -1 and 1
+		    randomvariation = randomvariation * direction      #Then multiplies it by the float
+		    self.phenotype[0] += randomvariation                                              #And adds that value
+		newphenotype.append(self.phenotype[0])
 		
 		for t in self.phenotype[1:3]:	
 		    randomvariation = random.uniform(0,.5) #Picks a random float between 0 and .005
