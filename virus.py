911ac6589f0a42e7a4d15d4033c2b921877e2f6e
Brad <brad@ubuntu.(none)>
2013-11-03 18:52:10 -0500

    added virus movement

--- virus.py
+++ virus.py
@@ -8,6 +8,8 @@ class Virus(Cell):
     key =
     infection_potency =
     environment = 
+    bool_moving = FALSE
+    moving_path = []
 
     def keyGen(self):
         key = round(random.random(), 2)*100
@@ -20,11 +22,37 @@ class Virus(Cell):
         chance = infection_potency - victim.resistance
         if random.random() <= chance:
             clone = InfectedCell(victim, incubation_period, symptom_potency, key)
-            e.remove_cell(victim)
-            e.add_specific_cell_at(clone, victim.pos)
+            environment.remove_cell(victim)
+            environment.add_specific_cell_at(clone, victim.pos)
         if victim.guessedKey(key):
-            e.kill(self)
+            environment.kill(self)
 
+    def move_like_virus(self)
+        if !bool_moving:
+            bool_moving = TRUE
+            current_pos = self.pos
+            moving_path_as_string = "x^",curveTendency
+            end_of_eY = environment.height - current_pos.y
+            end_of_eX = environment.width - current_pos.x
+            distanceTraveled = 0
+            timeLeft = math.abs(round(curveTendency,0) + 1)
+            while timeLeft > 0:
+                nextPos = Point(current_pos.x + speed, (speed/math.abs(speed))*(speedcurrent_pos.x**curveTendency))
+                moving_path.append(nextPos)
+                current_pos = nextPos
+                timeLeft--
+        else:
+            ticksMax = move_path.length
+            ticksLeft = 0
+            while ticksLeft <= ticksMax:
+                self.pos.x = move_path[ticksLeft].x
+                self.ps.y = move_path[ticksLeft].y
+                ticksLeft++
+            bool_moving = FALSE
+
+    def one_tick(self):
+        self.move_like_virus()
+        self.life_and_destruction()
 
 class InfectedCell(Cell, cell, iPeriod, symPot, parentKey):
     
